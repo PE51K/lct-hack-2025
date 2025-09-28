9344a4f9191fb96337570affce5e00f88c210eaa
PE51K <grigorij1m@gmail.com>
2025-09-28 13:41:55 +0300

    style: format enum definitions for consistency

--- backend/models/extract.py
+++ backend/models/extract.py
@@ -18,13 +18,13 @@ SourceType = Enum(
     ],
 )
 ContentType = Enum(
-    "Content_type", 
+    "Content_type",
     [
-        ("na", 1), 
-        ("csv", 2), 
-        ("xml", 3), 
-        ("json", 4), 
-        ("table", 5), 
+        ("na", 1),
+        ("csv", 2),
+        ("xml", 3),
+        ("json", 4),
+        ("table", 5),
         ("parquet", 6)
     ]
 )
