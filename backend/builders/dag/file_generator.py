3e7bc56ab4a546d5d2f34ae5781e3d08e6420c70
PE51K <grigorij1m@gmail.com>
2025-10-02 21:54:24 +0300

    fix: Add noqa comment to suppress linting warning for autoescape setting in Jinja2 environment

--- backend/builders/dag/file_generator.py
+++ backend/builders/dag/file_generator.py
@@ -43,7 +43,7 @@ class AirflowFileGenerator:
             loader=FileSystemLoader(str(template_dir)),
             trim_blocks=True,
             lstrip_blocks=True,
-            autoescape=False,
+            autoescape=False, # noqa: S701
         )
 
         logger.info(f"AirflowFileGenerator initialized with output dir: {self.base_output_dir}")
